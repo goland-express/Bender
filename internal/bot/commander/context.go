ef71e1417baf32b68f944ba781aa764f09b2faa3
LordShadd <real.davidwilson@proton.me>
2025-10-30 16:07:40 -0300

    feat: set context fields

--- internal/bot/commander/context.go
+++ internal/bot/commander/context.go
@@ -4,14 +4,37 @@ import "github.com/bwmarrin/discordgo"
 
 type Context struct {
 	Session   *discordgo.Session
-	Messenger Messenger
+	GuildID   string
+	AuthorID  string
+	ChannelID string
+	Messenger *Messenger
 	Args      []string
 }
 
 func NewContext(session *discordgo.Session, messenger *Messenger, args []string) *Context {
 	return &Context{
 		Session:   session,
-		Messenger: *messenger,
+		Messenger: messenger,
 		Args:      args,
 	}
+}
+
+func (ctx *Context) SetSession(session *discordgo.Session) {
+	ctx.Session = session
+}
+
+func (ctx *Context) SetGuildID(guildID string) {
+	ctx.GuildID = guildID
+}
+
+func (ctx *Context) SetAuthorID(authorID string) {
+	ctx.AuthorID = authorID
+}
+
+func (ctx *Context) SetChannelID(channelID string) {
+	ctx.ChannelID = channelID
+}
+
+func (ctx *Context) SetMessenger(messenger *Messenger) {
+	ctx.Messenger = messenger
 }
